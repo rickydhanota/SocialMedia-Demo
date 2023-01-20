7ba827a1d7e75ba991840f0655db57b0f689217b
Ricky <rickdhanota@gmail.com>
2023-01-20 00:27:20 -0800

    issues with my register form

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
         textAlign="center"
       >
         <Typography fontWeight="bold" fontSize="32px" color="primary">
-          Sociopedia
+          Rickypedia
         </Typography>
       </Box>
 
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Socipedia, the Social Media for Sociopaths!
+          Welcome to Rickypedia, the Social Media Platform for Ricky!
         </Typography>
         <Form />
       </Box>
